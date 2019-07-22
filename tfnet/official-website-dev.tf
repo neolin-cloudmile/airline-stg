47f295cd1eb10ba62fcbdfdcfdd87aedc9fa6c86
Neo Lin <neo.lin@mile.cloud>
2019-07-22 09:31:03 +0800

    Add new subnet to network

--- tfnet/official-website-dev.tf
+++ tfnet/official-website-dev.tf
@@ -3,11 +3,43 @@
   name = "official-website-dev"
   auto_create_subnetworks = "false"
 }
-# Create official-website-dev subnetwork
+# Create public-subnet-1 subnetwork
 resource "google_compute_subnetwork" "public-subnet-1" {
   name          = "public-subnet-1"
   region        = "asia-east1"
-  network       = "${google_compute_network.official-website-dev-network.self_link}"
+  network       = "${google_compute_network.official-website-dev.self_link}"
   ip_cidr_range = "10.240.66.0/24"
+  private_ip_google_access = "true"
+}
+# Create public-subnet-2 subnetwork
+resource "google_compute_subnetwork" "public-subnet-2" {
+  name          = "public-subnet-2"
+  region        = "asia-east1"
+  network       = "${google_compute_network.official-website-dev.self_link}"
+  ip_cidr_range = "10.240.68.0/24"
+  private_ip_google_access = "true"
+}
+# Create private-subnet-1 subnetwork
+resource "google_compute_subnetwork" "private-subnet-1" {
+  name          = "public-subnet-1"
+  region        = "asia-east1"
+  network       = "${google_compute_network.official-website-dev.self_link}"
+  ip_cidr_range = "10.240.70.0/24"
+  private_ip_google_access = "true"
+}
+# Create private-subnet-2 subnetwork
+resource "google_compute_subnetwork" "private-subnet-2" {
+  name          = "private-subnet-2"
+  region        = "asia-east1"
+  network       = "${google_compute_network.official-website-dev.self_link}"
+  ip_cidr_range = "10.240.72.0/24"
+  private_ip_google_access = "true"
+}
+# Create private-subnet-k8s subnetwork
+resource "google_compute_subnetwork" "private-subnet-k8s" {
+  name          = "private-subnet-k8s"
+  region        = "asia-east1"
+  network       = "${google_compute_network.official-website-dev.self_link}"
+  ip_cidr_range = "10.240.76.0/23"
   private_ip_google_access = "true"
 }
